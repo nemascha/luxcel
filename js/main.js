274b1bf1e43612aa1a29ad7fbef1aaaa65b5032c
Mikalai Bury <burymm@gmail.com>
2017-05-13 09:33:49 +0300

    update js for left menu

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     document.querySelector('#side-menu-button').addEventListener('click', function() {
         if (document.body.className.includes('side-menu')) {
-            document.body.className = document.body.className.replace('side-menu', '');
+            document.body.className = document.body.className.replace(' side-menu', '');
         } else {
             document.body.className += ' side-menu';
         }
